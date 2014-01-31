b0c2f096df928de222712afbc3d83aca371e53b5
Aron Roberts <aron@socrates.berkeley.edu>
2014-01-31 14:54:51 -0800

    CSPACE-6273: Remove now-extraneous manifest, template, and test file.

--- tests/env_java_home.pp
+++
@@ -1,4 +0,0 @@
-class { 'cspace_user::env_java_home': }
-
-$default_home = $cspace_user::env_java_home::default_java_home
-notice( "Default value of JAVA_HOME environment variable is ${default_home}" )
